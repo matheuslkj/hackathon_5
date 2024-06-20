b676ad4b514d1b9230eaa1b2e0d79a75fa022ac9
alnsousa1 <alnprivado@gmail.com>
2024-06-19 21:44:07 -0300

    organizando rotas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\Api\v1\ResponsavelController;
 use App\Http\Controllers\Api\v1\IdosoController;
+use App\Http\Controllers\api\v1\ProfissionalSaudeController;
 use App\Http\Controllers\api\v1\VacinaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,15 +12,7 @@ Route::get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'v1'], function(){
     Route::apiResource('responsavels', ResponsavelController::class);
-});
-
-Route::group(['prefix' => 'v1'], function(){
     Route::apiResource('idosos', IdosoController::class);
-});
-
-Route::group(['prefix' => 'v1'], function(){
     Route::apiResource('vacinas', VacinaController::class);
+    Route::apiResource('profissionais', ProfissionalSaudeController::class);
 });
-// Route::group(['prefix' => 'v1'], function(){
-//     Route::apiResource('profissionais', ProfissionalSaudeController::class);
-// });
